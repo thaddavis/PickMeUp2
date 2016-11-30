105ed2e0b774206e3f3c62bfb3b1979e8b77661d
thaddavis <thaddadavis@gmail.com>
2016-11-30 10:36:54 -0500

    working on ios emulator

---
+++ src/components/map/map.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var google:any;
+
+@Component({
+  selector: 'map',
+  templateUrl: 'map.html'
+})
+export class MapComponent implements OnInit {
+  public map: any;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.map = this.createMap();
+  }
+
+  createMap(location = new google.maps.LatLng(40.7127, -74.0059)) {
+    let mapOptions = {
+      center: location,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true
+    }
+
+    let mapEl = document.getElementById('map');
+    let map =  new google.maps.Map(mapEl, mapOptions);
+
+    return map;
+  }
+
+}
