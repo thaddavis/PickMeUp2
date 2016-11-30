fe024f9b24e3aa4b82b21bfb4eb10d751336a847
thaddavis <thaddadavis@gmail.com>
2016-11-30 13:26:43 -0500

    added geolocation working on ios emulator

--- src/components/map/map.ts
+++ src/components/map/map.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Geolocation } from 'ionic-native';
+import { Observable } from 'rxjs/Observable';
+import { LoadingController, NavController } from 'ionic-angular';
 
 declare var google:any;
 
@@ -11,10 +14,51 @@ export class MapComponent implements OnInit {
   @Input() isPickupRequested: boolean;
   public map: any;
 
-  constructor() {}
+  constructor( public navCtrl: NavController, public loadingCtrl: LoadingController ) {}
 
   ngOnInit() {
     this.map = this.createMap();
+
+    this.getCurrentLocation().subscribe(location => {
+      this.centerLocation(location);
+    })
+  }
+
+  getCurrentLocation() {
+
+    let loading = this.loadingCtrl.create({
+      content: "Loading"
+    })
+    loading.present();
+
+    let options = { timeout: 10000, enableHighAccuracy: true };
+    let locationObs = Observable.create(observable => {
+      Geolocation.getCurrentPosition(options)
+        .then( resp => {
+          let lat = resp.coords.latitude;
+          let lng = resp.coords.longitude;
+          let location = new google.maps.LatLng(lat,lng);
+
+          observable.next(location);
+          loading.dismiss();
+        },
+        (err) => {
+          console.log("Geolocation err: " + err);
+          loading.dismiss();
+        });
+    })
+
+    return locationObs;
+  }
+
+  centerLocation(location) {
+    if ( location ) {
+      this.map.panTo(location);
+    } else {
+      this.getCurrentLocation().subscribe(currentLocation => {
+        this.map.panTo(currentLocation);
+      });
+    }
   }
 
   createMap(location = new google.maps.LatLng(40.7127, -74.0059)) {
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
@@ -6,11 +6,13 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   public isPickupRequested: boolean;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
+
+  ngOnInit() {
     this.isPickupRequested = false;
   }
 
